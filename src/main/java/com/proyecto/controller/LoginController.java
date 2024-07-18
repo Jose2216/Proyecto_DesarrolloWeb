0bfc94b1cf945442eded45e8fa38e4cd9dea4040
Lidietholivar98 <lolivar40908@ufide.ac.cr>
2024-07-18 00:20:52 -0600

    Agregando paginas de login y crear cuenta

--- src/main/java/com/proyecto/controller/LoginController.java
+++ src/main/java/com/proyecto/controller/LoginController.java
@@ -1,12 +1,22 @@
 package com.proyecto.controller;
 
+import com.proyecto.service.LoginService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
-@RequestMapping("/login")
+@RequestMapping("/usuario")
 public class LoginController {
+    @Autowired
+    private LoginService loginService;
     
+    @GetMapping("/crearCuenta")
+    public String inicio(Model model){
+        return "/usuario/crearCuenta";
+    }
 }
