60512c52701f55d5de9ed1632462e413b8ece71e
Lidietholivar98 <lolivar40908@ufide.ac.cr>
2024-08-10 23:02:26 -0600

    agregando services, controllers, dominios para la administración de cuentas de usuario, además de las páginas correspondientes, aún me queda pendiente la navegación por las diferentes páginas, terminar la funcionalidad del login y otros forms, además de la apariencia de las páginas para que siga el diseño de Pempot

---
+++ src/main/java/com/proyecto/service/UsuarioService.java
@@ -0,0 +1,15 @@
+package com.proyecto.service;
+
+import com.proyecto.domain.Usuario;
+import java.util.List;
+
+public interface UsuarioService {
+    public List<Usuario> getUsuarios();
+    public Usuario getUsuario(Usuario usuario);
+    public Usuario getUsuarioPorUsername(String username);
+    public Usuario getUsuarioPorUsernameYPassword(String username, String password);
+    public Usuario getUsuarioPorUsernameOCorreo(String username, String correo);
+    public boolean existeUsuarioPorUsernameOCorreo(String username, String correo);
+    public void save(Usuario usuario, boolean crearRolUser);
+    public void delete(Usuario usuario);
+}
--- src/main/java/com/proyecto/service/impl/LoginServiceImpl.java
+++ src/main/java/com/proyecto/service/impl/LoginServiceImpl.java
@@ -1,16 +1,16 @@
 package com.proyecto.service.impl;
 
-import com.proyecto.dao.LoginDao;
 import com.proyecto.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.proyecto.dao.UsuarioDao;
 
 @Service
 public class LoginServiceImpl implements LoginService{
     
     //@Autowired
-    private LoginDao loginDao;
+    private UsuarioDao loginDao;
     
     @Override
     //@Transactional(readOnly=true)
